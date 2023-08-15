686af196b5c83610560cde78bdf0ee53f64eeca4
Bakhytbek <baxa.aseke@gmail.com>
2023-08-15 08:10:13 +0600

    Change send message

--- auth_user/service.py
+++ auth_user/service.py
@@ -10,7 +10,7 @@ bot = telebot.TeleBot(settings.TELEGRAM_BOT)
 
 
 def send_message(user: models.User, message: str) -> NoReturn:
-    if user.telegram_id:
+    if user.first_name:
         bot.send_message(
             user.telegram_id, constants.MESSAGE.format(name=user.first_name, message=message),
             parse_mode='Markdown'
